8e0791d20d6fbf9670fb08579bb933479615f1c8
Celio Barros <celio.barros.filho@gmail.com>
2018-05-06 17:39:27 -0300

    [CourseCard] Put information on course card

--- src/app/entities/course/course-card/course-card.component.ts
+++ src/app/entities/course/course-card/course-card.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CourseCardComponent implements OnInit {
   
-  @Input() course: object;
+  @Input() course: any;
 
   constructor() { }
 
   ngOnInit() {
   	console.log(this.course);
+  }
+
+  price(isPaid, price) {
+  	if (isPaid) {
+  		const value = parseFloat(price).toFixed(2).toString().replace(".", ",");
+		return `R$ ${value}`;
+  	} else {
+  		return "GRÁTIS";
+  	}
   }
 
 }
